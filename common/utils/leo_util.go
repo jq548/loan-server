4d73fca3fef6349a1a750f226bc1b1fad39a2141
jq <>
2025-01-14 15:28:26 +0800

    fix

--- common/utils/leo_util.go
+++ common/utils/leo_util.go
@@ -1,19 +1,11 @@
 package utils
 
 import (
-	"github.com/ethereum/go-ethereum/common"
 	"regexp"
 )
 
 // IsValidLeoAddress validate hex address
-func IsValidLeoAddress(iaddress interface{}) bool {
-	re := regexp.MustCompile("^aleo[0-9a-fA-F]{59}$")
-	switch v := iaddress.(type) {
-	case string:
-		return re.MatchString(v)
-	case common.Address:
-		return re.MatchString(v.Hex())
-	default:
-		return false
-	}
+func IsValidLeoAddress(iaddress string) bool {
+	re := regexp.MustCompile("^aleo[0-9a-zA-Z]{59}$")
+	return re.MatchString(iaddress)
 }
