af5ab0e4f9b72dc9fedf8d333ca6284a283343d6
JQ <TCDSLiuJQ@live.com>
2025-02-19 09:49:48 +0800

    fix

--- common/utils/smtp_util_test.go
+++ common/utils/smtp_util_test.go
@@ -6,13 +6,13 @@ func TestSendEmail(t *testing.T) {
 
 	email, err := SendEmail(
 		false,
-		"liamc1878@gmail.com",
-		"liamc1878@gmail.com",
-		"fhqx ayeq fmii waoh",
+		"",
+		"",
+		"",
 		"smtp.gmail.com",
 		587,
-		"1140830756@qq.com",
-		"hello",
+		"",
+		"",
 	)
 	if err != nil {
 		t.Error(err)
